cc3461fdb9ff3523ef882be1c6f454ce855f1c4e
yanshengge <yanshengge@yangqianguan.com>
2020-05-28 17:02:47 +0800

    Code Format

--- 2.两数相加.swift
+++ 2.两数相加.swift
@@ -18,31 +18,30 @@
  */
 class Solution {
   func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-    
     guard let list1 = l1 else {
       return l2
     }
-    guard  let list2 = l2 else {
+    guard let list2 = l2 else {
       return l1
     }
-    
+
     var l1Node: ListNode? = list1
     var l2Node: ListNode? = list2
     var tempSum: Int = 0
     let resultNode: ListNode = ListNode(0)
     var resultTemp = resultNode
-    
+
     while l1Node != nil || l2Node != nil || tempSum != 0 {
       let node1Value = l1Node?.val ?? 0
       let node2Value = l2Node?.val ?? 0
-      
+
       let itemSum = node1Value + node2Value + tempSum
       tempSum = itemSum / 10
-      
+
       let sumNode = ListNode(itemSum % 10)
       resultTemp.next = sumNode
       resultTemp = sumNode
-      
+
       if l1Node != nil {
         l1Node = l1Node?.next
       }
@@ -54,5 +53,5 @@ class Solution {
     return resultNode.next
   }
 }
-// @lc code=end
 
+// @lc code=end
